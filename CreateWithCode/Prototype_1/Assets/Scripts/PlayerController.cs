76b55e0fa66bbd5edfa283812ea99828dbe9e67f
JellyQueen44 <113132111+JellyQueen44@users.noreply.github.com>
2022-09-20 10:22:25 -0700

    The car now rotates properly and hierarchy cleaned up(initial tutorial finished)

--- CreateWithCode/Prototype_1/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype_1/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    //private vehicles mechanics
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //The player input is here
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         
-        //Car will sure be movin someday
+        //Car is movin! Wowie, this moves the car
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        // And this does, in fact, turn the car
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
