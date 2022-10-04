e5d2343e7cc77d90009eed1ec8f8016e8129c6a7
JellyQueen44 <113132111+JellyQueen44@users.noreply.github.com>
2022-10-04 10:42:05 -0700

    Spawns are now randomized and clean

--- CreateWithCode/Prototype_2/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype_2/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefab;
-    public int animalIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,10 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S)) 
         {
-            Instantiate(animalPrefab[animalIndex], new Vector3(0, 0, 20), animalPrefab[animalIndex].transform.rotation);
+            int animalIndex = Random.Range(0, animalPrefab.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+            Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
         }
     }
 }
