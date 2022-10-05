12ff57ee22bc360c0bde206d0b83f135c2b46d19
JellyQueen44 <113132111+JellyQueen44@users.noreply.github.com>
2022-10-05 10:34:10 -0700

    Animals now randomly spawn independently

--- CreateWithCode/Prototype_2/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype_2/Assets/Scripts/SpawnManager.cs
@@ -7,21 +7,24 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefab;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float spawnDelay = 2f;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", spawnDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) 
-        {
-            int animalIndex = Random.Range(0, animalPrefab.Length);
+        
+    }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefab.Length);
             Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
             Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
-        }
     }
 }
