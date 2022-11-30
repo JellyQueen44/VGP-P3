4ee1452dfa09fffb6ee6a3ad32f0f5b741d44613
JellyQueen44 <113132111+JellyQueen44@users.noreply.github.com>
2022-11-30 10:38:10 -0800

    Personal Project Movement and Boundaries Added

--- CreateWithCode/Lab_2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Lab_2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     private float speed = 10f;
     private Rigidbody playerRb;
+    private float zBound = 6.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +18,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        ConstraintMove();
+    }
+    
+    void MovePlayer()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+        
+    }
+
+    void ConstraintMove()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+            if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
     }
 }
