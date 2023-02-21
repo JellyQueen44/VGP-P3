6d17125ddfbb9322dcf27851fb9bb5f20fdd3d4d
JellyQueen44 <113132111+JellyQueen44@users.noreply.github.com>
2023-02-21 10:39:08 -0800

    Boss Spawns Properly Every 3 Waves + Begun Work On Second Powerup

--- CreateWithCode/Prototype_4/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype_4/Assets/Scripts/SpawnManager.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] enemyPrefab;
-    public GameObject bossEnemy;
+    public GameObject[] bossPrefab;
     public GameObject powerupPrefab;
     public float spawnRange = 9.0f;
     public int enemyCount;
-    public int bossCount;
     public int waveNumber = 1;
     // Start is called before the first frame update
     void Start()
@@ -22,19 +21,15 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         enemyCount = FindObjectsOfType<Enemy>().Length;
-        bossCount = FindObjectsOfType<Enemy>().Length;
-
         if(enemyCount == 0)
         {
             waveNumber++;
             SpawnEnemyWave(waveNumber);
             Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+
+            SpawnBossWave(1);
         }
-        
-        if(waveNumber % 3 == 0 && bossCount == 0)
-        {
-            Instantiate(bossEnemy, GenerateSpawnPosition(), bossEnemy.transform.rotation);
-        }
+
     }
 
     void SpawnEnemyWave(int enemiesToSpawn)
@@ -42,7 +37,23 @@ public class SpawnManager : MonoBehaviour
         for (int i = 0; i < enemiesToSpawn; i++)
         {
             int enemyIndex = Random.Range(0, enemyPrefab.Length);
+            
             Instantiate(enemyPrefab[enemyIndex], GenerateSpawnPosition(), enemyPrefab[enemyIndex].transform.rotation);
+
+            
+        }
+    }
+
+    void SpawnBossWave(int bossToSpawn)
+    {
+        int bossIndex = Random.Range(0, bossPrefab.Length);
+
+        for (int i = 0; i < bossToSpawn; i++)
+        {
+            if(waveNumber % 3 == 0)
+            {
+                Instantiate(bossPrefab[bossIndex], GenerateSpawnPosition(), bossPrefab[bossIndex].transform.rotation);
+            }
         }
     }
     
