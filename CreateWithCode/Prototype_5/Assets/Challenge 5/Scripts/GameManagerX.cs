951b20d3e7271764ec70d7b454d2edcfa025f889
JellyQueen44 <113132111+JellyQueen44@users.noreply.github.com>
2023-03-09 10:36:48 -0800

    Difficulty Buttons Adjust Difficulty Of Game + Restart Button Activates On Game Over + Score Properly Displays w/Current Score Amount + Objects Only Destroy On Click

--- CreateWithCode/Prototype_5/Assets/Challenge 5/Scripts/GameManagerX.cs
+++ CreateWithCode/Prototype_5/Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -23,15 +23,7 @@ public class GameManagerX : MonoBehaviour
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
     
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
-    public void StartGame()
-    {
-        spawnRate /= 5;
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
-        titleScreen.SetActive(false);
-    }
+    
 
     // While game is active spawn a random target
     IEnumerator SpawnTarget()
@@ -70,14 +62,14 @@ public class GameManagerX : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "score";
+        scoreText.text = "Score: " + score;
     }
 
     // Stop game, bring up game over text and restart button
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
-        restartButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(true);
         isGameActive = false;
     }
 
@@ -85,6 +77,16 @@ public class GameManagerX : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        titleScreen.SetActive(false);
     }
 
 }
